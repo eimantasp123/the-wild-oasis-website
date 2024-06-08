dddd337b59849859527d3149d09216d6c909d772
Eimantas <eimantaspetrikas18@gmail.com>
2024-06-08 03:18:46 +0200

    Initial commit

---
+++ app/_lib/actions.js
@@ -0,0 +1,107 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
+
+export async function updateProfile(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) throw new Error("Please provide a valid national ID");
+  const updateDate = { nationality, countryFlag, nationalID };
+  const { error } = await supabase.from("guest").update(updateDate).eq("id", session.user.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
+  revalidatePath("/account/profile");
+}
+
+export async function createBooking(bookingData, formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: "unconfirmed",
+  };
+
+  const { error } = await supabase.from("bookings").insert([newBooking]);
+  if (error) throw new Error("Booking could not be created");
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`);
+}
+
+export async function deleteReservation(bookingId) {
+  // await new Promise((res) => setTimeout(res, 2000));
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsId = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsId.includes(bookingId)) throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase.from("bookings").delete().eq("id", bookingId);
+  if (error) throw new Error("Booking could not be deleted");
+
+  revalidatePath("/account/reservations");
+}
+
+export async function updateReservation(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsId = guestBookings.map((booking) => booking.id);
+  const bookingId = Number(formData.get("bookingId"));
+  if (!guestBookingsId.includes(bookingId)) throw new Error("You are not allowed to update this booking");
+
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  const { error } = await supabase.from("bookings").update(updateData).eq("id", bookingId);
+  if (error) throw new Error("Booking could not be updated");
+
+  revalidatePath("/account/reservations");
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  redirect("/account/reservations");
+}
+
+/////////////
+// UPDATE
+
+export async function updateBooking(id, updatedFields) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .update(updatedFields)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be updated");
+  }
+  return data;
+}
+
+export async function signInAction() {
+  await signIn("google", { redirectTo: "/account" });
+}
+
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
+}
---
+++ app/layout.js
@@ -0,0 +1,36 @@
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+import { ReservationProvider } from "./_components/ReservationContext";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: {
+    template: "%s - The Wild Oasis",
+    default: "Welcome / The wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel loacted in the hearof the Italian Dolomites, surrounded by beautiful mountains and dar forests",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen relative flex flex-col`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-11 grid">
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
